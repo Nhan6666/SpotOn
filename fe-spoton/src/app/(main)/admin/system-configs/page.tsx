bd745cec82eb9a321ea3d86306c29ab5e96fea0c
NamDV <namdv.ce190076@gmail.com>
2026-06-05 16:22:11 +0700

    update vi.json

--- fe-spoton/src/app/(main)/admin/system-configs/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { SystemConfigsFeature } from '@/features/system-configs/SystemConfigsFeature';
-
-export default function SystemConfigsPage() {
-  return <SystemConfigsFeature />;
-}

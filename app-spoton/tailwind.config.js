b28cb56489f225102c57f5c62c06ad71faffd829
khanhdc2901 <duykdc2901@gmail.com>
2026-07-21 17:36:35 +0700

    style: Increase global font sizes for better readability

--- app-spoton/tailwind.config.js
+++ app-spoton/tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
       },
       fontFamily: {
         lexend: ["Lexend", "sans-serif"],
+      },
+      fontSize: {
+        'xs': ['13px', { lineHeight: '18px' }],
+        'sm': ['15px', { lineHeight: '22px' }],
+        'base': ['17px', { lineHeight: '24px' }],
+        'lg': ['19px', { lineHeight: '26px' }],
+        'xl': ['22px', { lineHeight: '28px' }],
+        '2xl': ['26px', { lineHeight: '32px' }],
+        '3xl': ['32px', { lineHeight: '38px' }],
+        '4xl': ['38px', { lineHeight: '44px' }],
       }
     },
   },

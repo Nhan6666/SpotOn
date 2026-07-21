d6981a4c2517601f606133be0691ca9ce435a90a
Nhan9999 <nhan.ce190122@gmail.com>
2026-07-21 17:11:02 +0700

    add

--- spoton-backend/verify_all.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  await mongoose.connection.collection('users').updateMany({}, { $set: { is_email_verified: true } });
-  console.log('Updated all users');
-  process.exit(0);
-});
